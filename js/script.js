a7ada93515824105eba4121ff960775fc2a2906a
Caiopachec <caiopacheco936@gmail.com>
2026-03-25 23:19:38 -0300

    Adiciona seções "Produtos" e "Footer" com conteúdo e estilos correspondentes; ajusta estilos existentes para responsividade

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ mobileBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-times');
 
 });
+
+
+window.addEventListener("scroll", () => {
+    const header = document.getElementById("header");
+
+    if (window.scrollY > 0) {
+        header.style.boxShadow = "0 4px 10px rgba(0, 0, 0, 0.1)";
+    } else {
+        header.style.boxShadow = "none";
+    }
+});
